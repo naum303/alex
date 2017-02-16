dcf5dde909fda01b3c2ca456aa11059605e1fdfe
Markus Naumann <m.naumann@lvm.de>
2017-02-16 14:51:35 +0100

    Sparten ergaenzt

---
+++ src/main/java/de/alex/types/Sparte.java
@@ -0,0 +1,15 @@
+package de.alex.types;
+
+public enum Sparte {
+
+	HAFTPFLICHT,
+	HAUSRAT,
+	WOHNGEBAEUDE,
+	KRAFTFAHRT,
+	LEBEN,
+	KRANKEN,
+	UNFALL,
+	RECHTSSCHUTZ,
+	FONDS,
+	AUSLANDSREISEKRANKEN
+}
